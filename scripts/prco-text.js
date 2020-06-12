0fc6fd70d95abac4e3b1254f203ebf83bf20e2a4
Frank Gutierrez <dearfrankg@gmail.com>
2020-06-12 01:04:20 -0700

    refactor

--- scripts/prco-text.js
+++ scripts/prco-text.js
@@ -2,32 +2,34 @@ const npm = require("npm");
 const { getOptions } = require("../src/config/get-options");
 const { twilioSend, signalwireSend } = require("../src/text-outbound");
 
-const handleCommand = async (options) => {
-  const isServerCommand = !!options.serverCommand;
+const isRunningTests = typeof jest !== "undefined";
 
-  if (isServerCommand) {
-    // INBOUND
-    const isServerStart = options.serverCommand === "start";
+const handleInbound = async (options) => {
+  const isServerStart = options.serverCommand === "start";
 
-    console.log(options);
+  isServerStart ? npm.load(() => npm.run("server-start")) : npm.load(() => npm.run("server-stop"));
 
-    isServerStart
-      ? npm.load(() => npm.run("server-start"))
-      : npm.load(() => npm.run("server-stop"));
-
-    return true;
-  } else {
-    // OUTBOUND
-    const response = options.isUsingTwilio
-      ? await twilioSend(options)
-      : await signalwireSend(options);
-
-    console.log(response);
-
-    return response;
-  }
+  return options;
 };
 
-const prcoText = () => Promise.resolve().then(getOptions).then(handleCommand);
+const handleOutbound = async (options) => {
+  const response = options.isUsingTwilio
+    ? await twilioSend(options)
+    : await signalwireSend(options);
+
+  if (!isRunningTests) console.log(response);
+
+  return response;
+};
+
+const prcoText = () => {
+  return Promise.resolve()
+    .then(getOptions)
+    .then((options) => {
+      const isServerCommand = !!options.serverCommand;
+
+      return isServerCommand ? handleInbound(options) : handleOutbound(options);
+    });
+};
 
 prcoText();
